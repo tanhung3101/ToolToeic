cbf7990239d6b8762872fa5b0528fc0c691bb83a
tanhung3101 <tanhung3101@gmail.com>
2014-05-13 00:01:57 +0700

    update

--- src/Game2.java
+++ src/Game2.java
@@ -1,8 +1,34 @@
+import info.clearthought.layout.TableLayout;
 
-public class Game2 {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class Game2  extends JFrame{
 	public static void main (String[] args){
 		//first change
+		 JFrame frame = new Game2();
+	        frame.setVisible(true);
+	        frame.setSize(300, 300);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		System.out.println();
+	}
+	public Game2(){
+		this.add(createPanel());
+	}
+	
+	
+	public JPanel createPanel(){
+		JPanel mainPanel=new JPanel();
+		JLabel lblQuestion=new JLabel("AAAAAAAA");
+		lblQuestion.setText("adsasdasds");
+		double[][] size={{0.5},{0.5}};
+		
+		mainPanel.setLayout(new TableLayout(size));
+		
+		mainPanel.add(lblQuestion,"0,0,0,0");
+		
+		return mainPanel;
 	}
 }
